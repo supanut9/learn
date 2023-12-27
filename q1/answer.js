2988337ddb6e99c84e27edf3b3c0f9e721028741
Supanut Tansorajprasert <supanut.tanso@gmail.com>
2023-12-27 12:34:45 +0700

    implement the answer of question 1 (less time complexity)

--- q1/answer.js
+++ q1/answer.js
@@ -1,9 +1,35 @@
+var runtime = function (func) {
+    var start = performance.now();
+    func();
+    var end = performance.now();
+    console.log("Execution time: ".concat(end - start, " ms"));
+};
 var Addup = function (arr) {
+    // maximum 16 unit
     var result = (parseInt(arr.join("")) + 1)
         .toString()
         .split("")
         .map(function (num) { return parseInt(num); });
     return result;
+};
+var Addup2 = function (arr) {
+    var status = false;
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (status) {
+            break;
+        }
+        if (arr[i] === 9) {
+            arr[i] = 0;
+            if (i === 0) {
+                arr.unshift(1);
+            }
+        }
+        else {
+            arr[i] += 1;
+            status = true;
+        }
+    }
+    return arr;
 };
 /*
 input [2,3] return [2,4]
@@ -11,8 +37,18 @@ input [9] return [1,0]
 input [9,9,9] return [1,0,0,0]
 input [7,8,8] return [7,8,9]
 */
-var tests = [[2, 3], [9], [9, 9, 9], [7, 8, 8]];
+var tests = [
+    [2, 3],
+    [9],
+    [9, 9, 9],
+    [7, 8, 8],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3],
+];
 for (var _i = 0, tests_1 = tests; _i < tests_1.length; _i++) {
     var test = tests_1[_i];
-    console.log(Addup(test));
+    // console.log(
+    //   runtime(() => Addup(test)),
+    //   runtime(() => Addup2(test))
+    // );
+    console.log(Addup(test), Addup2(test));
 }
--- q1/answer.ts
+++ q1/answer.ts
@@ -1,10 +1,41 @@
+const runtime = (func) => {
+  const start = performance.now();
+  func();
+  const end = performance.now();
+  console.log(`Execution time: ${end - start} ms`);
+};
+
 const Addup = (arr: number[]): number[] => {
+  // maximum 16 unit
   const result = (parseInt(arr.join("")) + 1)
     .toString()
     .split("")
     .map((num) => parseInt(num));
 
   return result;
+};
+
+// O(n) -> Best Case n = 1
+const Addup2 = (arr: number[]): number[] => {
+  let status = false;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (status) {
+      break;
+    }
+
+    if (arr[i] === 9) {
+      arr[i] = 0;
+
+      if (i === 0) {
+        arr.unshift(1);
+      }
+    } else {
+      arr[i] += 1;
+
+      status = true;
+    }
+  }
+  return arr;
 };
 
 /*
@@ -14,8 +45,18 @@ input [9,9,9] return [1,0,0,0]
 input [7,8,8] return [7,8,9]
 */
 
-const tests = [[2, 3], [9], [9, 9, 9], [7, 8, 8]];
+const tests = [
+  [2, 3],
+  [9],
+  [9, 9, 9],
+  [7, 8, 8],
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3],
+];
 
 for (const test of tests) {
-  console.log(Addup(test));
+  // console.log(
+  //   runtime(() => Addup(test)),
+  //   runtime(() => Addup2(test))
+  // );
+  console.log(Addup(test), Addup2(test));
 }
